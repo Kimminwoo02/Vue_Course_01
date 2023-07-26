77f7471fd49a69b7de67ca8f9aee9d5c870eae73
Minwoo <made_power@naver.com>
2023-07-26 17:41:13 +0900

    vue 메서드
    보간법을 통한 메서드 추가

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app = Vue.createApp({
             courseGoal: 'Learn Vue',
             vueLink: 'http://vuejs.org/'
         };
+    },
+    
+    methods: {
+        outputGoal(){
+            const randomNumber  =Math.random();
+            if(randomNumber <0.5){
+                return 'Learn vue';
+            } else {
+                return 'Master Vue!';
+            }
+            
+        }
     }
 });
 
