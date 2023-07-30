901b412d527bd05efff41f4a1bc9d2746692768b
Minwoo <made_power@naver.com>
2023-07-30 20:57:36 +0900

    Vue의 기본 동적 스타일링
    Vue는 기본으로 동적으로 css 클래스를 추가할 수 있다.

--- app.js
+++ app.js
@@ -1,68 +1,23 @@
 const app = Vue.createApp({
-  data() {
-    return {
-      counter: 0,
-      name: '',
-      confirmedName: '',
-      fullname:'',
-      lastName:''
-
-
+   data(){
+    return{
+        boxASelected: false,
+        boxBSelected: false,
+        boxCSelected: false,
     };
-  },
-  watch:{
-    counter(value){
-      if (value> 50){
-        this.counter = 0
-      }
     },
-      
-  
-    lastName(value){
-      if(value ===''){
-        this.fullname = '';
-      }else{
-        this.fullname = this.name+ ' ' + value;
-      }
+    methods:{
+        boxSelected(box) {
+            if(box ==='A'){
+                this.boxASelected = true;
+            } else if (box ==='B'){
+                this.boxBSelected = true;
+            }else if (box ==='C'){
+                this.boxCSelected = true;
+            }
+
+        }
     }
-  },
-  computed:{
-    fullname(){
-      console.log('Running again');
-      if(this.name ==='' || this.lastName ===''){
-        return ''
-      }
-      return this.name + ' ' + this.lastName;
-    }
-  },
-  methods:{
-    outputFullname(){
-      if(this.name ===''){
-        return '';
-      }
-      return this.name + ' ' + 'Minu';
-    },
-    confirmInput(){
-      this.confirmedName=this.name;
-    },
-    submitForm(event){
-      
-      alert('submitted');
-    }
-    ,
-    setName(event,lastName){
-      this.name = event.target.value
-    },
-    add(num){
-        this.counter = this.counter + num;
-    },
-    reduce(num){
-        this.counter = this.counter - num;
-    },
-    resetInput(){
-      this.name = '';
-    }
-  }
 });
 
-app.mount('#events');
+app.mount('#styling');
