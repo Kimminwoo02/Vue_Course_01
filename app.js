f07cdb96e5fffd79d9e0b291d08ff829bbe61d6d
Minwoo <made_power@naver.com>
2023-07-30 21:07:41 +0900

    클래스를 이용한 동적 스타일링

--- app.js
+++ app.js
@@ -9,15 +9,16 @@ const app = Vue.createApp({
     methods:{
         boxSelected(box) {
             if(box ==='A'){
-                this.boxASelected = true;
+                this.boxASelected = !this.boxASelected;
             } else if (box ==='B'){
-                this.boxBSelected = true;
+                this.boxBSelected = !this.boxBSelected;
             }else if (box ==='C'){
-                this.boxCSelected = true;
+                this.boxCSelected = !this.boxCSelected;
             }
 
         }
     }
 });
+
 
 app.mount('#styling');
