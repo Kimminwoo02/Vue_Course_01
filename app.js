b417b89c07fdc165b40c7cb65ed85caec7235cd1
Minwoo <made_power@naver.com>
2023-08-08 20:28:09 +0900

    컴포넌트
    컴포넌트를 이해하기 위한 예제를 작성하였다.

--- app.js
+++ app.js
@@ -1,51 +1,28 @@
 const app = Vue.createApp({
-  data() {
+  data(){
     return {
-      currentUserInput: '',
-      message: 'Vue is great!',
-    };
-  },
-  methods: {
-    saveInput(event) {
-      this.currentUserInput = event.target.value;
-    },
-    setText() {
-      ///this.message = this.currentUserInput;
-      this.message = this.$refs.userText;
-    },
-    beforeCreate(){
-      console.log('beforeCreate()');
-    },
-    created(){
-      console.log('created()');
-    },
-    boforeMount(){
-      console.log('beforeMount()');
-    },
-    mounted(){
-      console.log('mounted()');
-    },
-    beforeUpdated(){
-      console.log('beforeUpdated');
-    },
-    update(){
-      console.log('updated()');
+      detailsAreVisible: false,
+      detailsAreVisibleB: false,
+      friends: [
+        {id:'manuel', 
+         name: 'Manuel Lorenz', 
+         phone: '01234 5678 999', 
+         email: 'asdfjgn@naver.com'
+        },
+        {
+          id: 'julie',
+          name: 'julie jones',
+          phone: '01234 5432 143',
+          email: 'julie@localhost.com'
+        },
+      ]
     }
   },
+  methods: {
+    toggleDetails(id){
+      this.detailsAreVisible =!this.detailsAreVisible;
+    }
+  }
 });
 
 app.mount('#app');
-
-app.unmount();
-
-const app2= Vue.createApp({
-  template: `
-  <p>{{ favoriteMeal }}</p
-  `,
-  data() {
-    return{
-      favoriteMeal: 'Something'
-    };
-  }
-});
-app2.mount('#app2');
