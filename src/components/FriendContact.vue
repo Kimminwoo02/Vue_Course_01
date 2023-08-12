23a0894c9a5c219529e155d55880aee2fbe1647b
Minwoo <made_power@naver.com>
2023-08-12 22:56:31 +0900

    컴포넌트 추가
    컴포넌트를 만들고 App에서 import하여 다른 컴포넌트를 불러올 수 있다.

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,31 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails"> 디테일 Show</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone: </strong> {{friend.phone}}</li>
+            <li><strong>email: </strong> {{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+          detailsAreVisible: false,
+          friend: {
+            id: "manuel",
+            name: "Manuel Lorenz",
+            phone: "0123 4563 20",
+            email: "manuel@localhost.com",
+          },
+        };
+    },
+    methods:{
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+};
+</script>
