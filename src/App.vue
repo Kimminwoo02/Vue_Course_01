aee62ec3cf49e8b0f3ff73a84b04d752d1c616da
Minwoo <made_power@naver.com>
2023-08-12 22:47:23 +0900

    기본 App 생성
    App을 생성 후 기본 설정을 하였다. 특히 App.vue의 export default는 App이 import 되는 곳에 기본적으로  안에 내용을 사용한다는 뜻이다.

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+    <h2>My Friends</h2>
+    <ul>
+        <li></li>
+    </ul>
+</section>
+</template>
+
+<script>
+ export default {
+    data() {
+        return{
+            firends: [
+                {
+                    id: 'minu',
+                    name: 'Minu',
+                    phone: '0123 2345 3456',
+                    email : 'minu@naver.com'
+                },
+                {
+                    id: 'asfd',
+                    name: 'adsfd',
+                    phone: '0123 2345 3456',
+                    email : 'asfd@naver.com'
+                },
+            ]
+        }
+    },
+    
+    };
+</script>
